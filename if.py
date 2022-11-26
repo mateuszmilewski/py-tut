4998de57d39374003cc020c6873cdc05f3c8ac17
milewski.88@gmail.com <=>
2022-11-26 18:58:21 +0100

    if file.py added

---
+++ if.py
@@ -0,0 +1,11 @@
+def iif(x):
+    if x == 0:
+        print('x is zero')
+    elif x > 0:
+        print('x is bigger than zero')
+    elif x < 0:
+        print('x is less then zero')
+
+iif(0)
+iif(13)
+iif(-4)
