c6fc81befec625f46650a5662e0c1c851fbe9618
milewski.88@gmail.com <=>
2022-11-26 20:26:53 +0100

    added small doc to quick-lambda

--- quick-lambda.py
+++ quick-lambda.py
@@ -1,4 +1,8 @@
 def fooConstruct(num):
+    """
+        doc string
+        this is a bit simple foo quasi-foo-constructor based on lambda expression
+    """
     return lambda x: x + 2 * num
 
 foo = fooConstruct(17)
@@ -10,3 +14,6 @@ print(foo(2))
 foo = fooConstruct(-100)
 print(foo(1))
 print(foo(2))
+
+
+print( fooConstruct.__doc__)
