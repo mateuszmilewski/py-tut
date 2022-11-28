d68cb245640a7cdbc98a6b65e5add268a93666f6
milewski.88@gmail.com <=>
2022-11-28 23:19:26 +0100

    hworldclass.py update

--- hworldclass.py
+++ hworldclass.py
@@ -13,6 +13,13 @@ class C2:
 
     def __init__(self, name):
         self.name = name    # instance variable unique to each instance
+        self.list = [] # correct way of making instance array
+
+    def addElement(self, element):
+        self.list.append(element)
 
 i2 = C2("abc")
 print(i2.cv, i2.name)
+i2.addElement('element oooo')
+
+print(i2.list)
